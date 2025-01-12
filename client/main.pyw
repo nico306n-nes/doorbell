cdd443c1681547f0701273de3d6b472bf0b6bd77
penewold <nico306n@nysted-efterskole.dk>
2025-01-12 23:08:45 +0100

    added sound capabilities

---
+++ client/main.pyw
@@ -0,0 +1,12 @@
+import window
+import sound
+
+text = window.Text("Der er nogen ved klokken :)")
+box = window.Box(400, 200, 10, "background2.png")
+jingle = sound.Sound("jingle.mp3")
+
+
+# IMPORTANT: play the jingle before opening the window
+jingle.play()
+window.create_window(text, box)
+
---
+++ client/sound.py
@@ -0,0 +1,30 @@
+import pygame
+
+pygame.init()
+pygame.mixer.init()
+
+
+
+class Sound:
+    sound = "jingle.mp3"
+    fade_in = 0
+
+    sound_sound = pygame.mixer.Sound(sound)
+
+    def __init__(self, sound):
+        self.sound_sound = pygame.mixer.Sound(sound)
+
+    def play(self, loops=-1, max_time=0, fade_ms=0):
+        self.sound_sound.play(loops, max_time, fade_ms)
+
+    def get_length(self):
+        return self.sound_sound.get_length()
+
+
+
+
+if __name__ == "__main__":
+    jingle = Sound("jingle.mp3")
+    jingle.play()
+
+    pygame.time.wait(int(jingle.get_length() * 1000 * 2))
