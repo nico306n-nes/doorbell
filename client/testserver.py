215e55e679d52a2b6ad0d6b798b2dd2d0ab808db
penewold <nico306n@nysted-efterskole.dk>
2025-01-13 11:45:43 +0100

    you can now emulate a button press on the test server by going to /emulate

--- client/testserver.py
+++ client/testserver.py
@@ -1,22 +1,35 @@
 from flask import Flask, jsonify, request
-import json
+import time
 
 app = Flask(__name__)
 
+start_time = round(time.time() * 1000)
+
 klokke_data = {
-	"current_time": 9834,
-	"clicked_time": 8944,
-	"diff": 890,
+	"current_time": round(time.time() * 1000) - start_time,
+	"clicked_time": -10000,
+	"diff": 0,
 	"format": "millis"
 }
+
+klokke_data["diff"] = klokke_data["current_time"] - klokke_data["clicked_time"]
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/emulate")
+def emulate():
+    klokke_data["clicked_time"] = round(time.time() * 1000) - start_time
+    return "<p>Emulated press</p>"
+
 @app.route("/klokke")
 def klokke():
+    klokke_data["current_time"] = round(time.time() * 1000) - start_time
+    klokke_data["diff"] = klokke_data["current_time"] - klokke_data["clicked_time"]
     return jsonify(klokke_data)
 
 if __name__ == "__main__":
+    print("Before")
     app.run(debug=True, port=8080)
+    print("After")
